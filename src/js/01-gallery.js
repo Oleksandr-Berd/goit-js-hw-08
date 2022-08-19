1883abc22cba25340ce936db44530fc8f2040955
Oleksandr <105509345+Oleksandr-Berd@users.noreply.github.com>
2022-08-19 20:26:49 +0300

    ііі

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,36 +3,3 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
-
-import SimpleLightbox from 'simplelightbox';
-
-import 'simplelightbox/dist/simple-lightbox.min.css';
-
-const lightboxLayout = createLayout(galleryItems);
-
-function createLayout(galleryItems) {
-  return galleryItems
-    .map(element => {
-      return `<a class="gallery__item" href="${element.original}">
-  <img class="gallery__image" src="${element.preview}" alt="${element.description}" />
-</a>`;
-    })
-    .join('');
-}
-
-const myGallery = document.querySelector('.gallery');
-
-myGallery.insertAdjacentHTML('afterbegin', lightboxLayout);
-
-myGallery.addEventListener('click', onClick, { once: true });
-
-let lightbox = new SimpleLightbox('.gallery__item', {
-  captionsData: `alt`,
-  captionDelay: 250,
-});
-
-function onClick(evt) {
-  evt.preventDefault();
-}
-
-console.log(myGallery);
