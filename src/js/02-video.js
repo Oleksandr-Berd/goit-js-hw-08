2e6d9d175a02227af1627f06241fe55874be3e42
Oleksandr <105509345+Oleksandr-Berd@users.noreply.github.com>
2022-08-20 17:26:02 +0300

    іі

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
+// import player from '@vimeo/player';
 
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('#vimeo-player');
+
+const player = new Vimeo.Player(iframe);
+
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(checkTime, 1000));
+
+function checkTime(evt) {
+  const message = JSON.stringify(evt);
+
+  localStorage.setItem(LOCALSTORAGE_KEY, message);
+}
+
+const time = localStorage.getItem(LOCALSTORAGE_KEY);
+
+const currentTime = JSON.parse(time);
+
+const seconds = currentTime.seconds;
+
+player.setCurrentTime(seconds).then(function (seconds) {
+  // seconds = the actual time that the player seeked to
+});
