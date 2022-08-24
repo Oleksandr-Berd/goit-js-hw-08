7afdad466cbad88446b6ace034aececb92f192a5
Oleksandr <105509345+Oleksandr-Berd@users.noreply.github.com>
2022-08-24 16:44:57 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -12,14 +12,30 @@ player.on('timeupdate', throttle(checkTime, 1000));
 
 function checkTime(evt) {
   const message = JSON.stringify(evt);
-
   localStorage.setItem(LOCALSTORAGE_KEY, message);
 }
 
 const time = localStorage.getItem(LOCALSTORAGE_KEY);
 
-const currentTime = JSON.parse(time);
+if (time !== null) {
+  const currentTime = JSON.parse(time);
 
-const seconds = currentTime.seconds;
+  const seconds = currentTime.seconds;
 
-player.setCurrentTime(seconds).then(function (seconds) {});
+  player
+    .setCurrentTime(seconds)
+    .then(function (seconds) {
+      // seconds = the actual time that the player seeked to
+    })
+    .catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          // the time was less than 0 or greater than the video’s duration
+          break;
+
+        default:
+          // some other error occurred
+          break;
+      }
+    });
+}
